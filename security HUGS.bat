dade77a2a9a21f43743433e474abad89a78501d3
fadin999 <hugoandreas22@gmail.com>
2024-08-22 21:44:08 +0200

    Add files via upload

---
+++ security HUGS.bat
@@ -0,0 +1,16 @@
+@echo off
+title SECURE UR PC FROM RAT
+color 2
+goto Greeting
+
+:Greeting
+cls
+echo G'day Nika!
+pause
+cls
+goto Secure
+
+:Secure
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+PING 1.1.1.1 -n 1 -w 0.1 >NUL
+goto Secure
